3cc6ff6edd0853e591014cc436795e57a5164076
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-09-07 12:35:58 -0300

    feat: aula tutorial para criação de projeto com netbeans

---
+++ aula_maven/src/main/java/com/pss/aula_maven/AulaMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pss.aula_maven;
+
+/**
+ *
+ * @author DAVI_
+ */
+public class AulaMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
